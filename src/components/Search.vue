0bf338b1eaaa1d5582ece5bcbce0a826bd353911
Alexander Nedomansky <anedomansky@googlemail.com>
2020-06-11 17:29:30 +0200

    Worked on the tab switching logic for the Search component.

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,16 +1,25 @@
 <template>
     <article class="search">
-        <div class="search__content">
-            Search works!
-        </div>
+        <button type="button" class="search__tab" @click="toggleTab(0)">Books</button>
+        <button type="button" class="search__tab" @click="toggleTab(1)">Games</button>
+        <button type="button" class="search__tab" @click="toggleTab(2)">Videos</button>
+        <div class="search__panel" v-show="activeTab === 0">Books</div>
+        <div class="search__panel" v-show="activeTab === 1">Games</div>
+        <div class="search__panel" v-show="activeTab === 2">Videos</div>
     </article>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-
+// TODO: style component
 @Component
-export default class Search extends Vue {}
+export default class Search extends Vue {
+    activeTab = 0;
+
+    toggleTab(tabNumber: number) {
+        this.activeTab = tabNumber;
+    }
+}
 </script>
 
 <style lang="scss" scoped>
