01dc8aaa8856ecac224eb696221c2456458038e8
Alexander Nedomansky <alexander.nedomansky@ica.de>
2020-12-19 10:32:24 +0100

    Added a placeholder container for the images on the DetailsPage. Added UNIQUE-constraint to all tables in order to prevent duplicate collection items.

--- main/db/index.ts
+++ main/db/index.ts
@@ -14,7 +14,7 @@ const db = new sqlite3.Database(dbFilename, (error): void => {
 });
 
 db.serialize(() => {
-    db.run('CREATE TABLE IF NOT EXISTS books (id INTEGER PRIMARY KEY AUTOINCREMENT, author_name TEXT, cover_i INTEGER, title TEXT, first_publish_year INTEGER)',
+    db.run('CREATE TABLE IF NOT EXISTS books (id INTEGER PRIMARY KEY AUTOINCREMENT, author_name TEXT, cover_i INTEGER, title TEXT UNIQUE, first_publish_year INTEGER)',
         (error): void => {
             if (error) {
                 console.trace(Colors.fgRed, '> Could not create the books table.', error, Colors.fgReset);
@@ -22,7 +22,7 @@ db.serialize(() => {
                 console.log(Colors.fgGreen, '> Created the books table.', Colors.fgReset);
             }
         });
-    db.run('CREATE TABLE IF NOT EXISTS games (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, background_image TEXT, released TEXT, rating INTEGER, rating_top INTEGER)',
+    db.run('CREATE TABLE IF NOT EXISTS games (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT UNIQUE, background_image TEXT, released TEXT, rating INTEGER, rating_top INTEGER)',
         (error): void => {
             if (error) {
                 console.trace(Colors.fgRed, '> Could not create the games table.', error, Colors.fgReset);
@@ -30,7 +30,7 @@ db.serialize(() => {
                 console.log(Colors.fgGreen, '> Created the games table.', Colors.fgReset);
             }
         });
-    db.run('CREATE TABLE IF NOT EXISTS shows (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, premiered TEXT, officialSite TEXT, status TEXT, summary TEXT, image_medium TEXT, image_original TEXT)',
+    db.run('CREATE TABLE IF NOT EXISTS shows (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT UNIQUE, premiered TEXT, officialSite TEXT, status TEXT, summary TEXT, image_medium TEXT, image_original TEXT)',
         (error): void => {
             if (error) {
                 console.trace(Colors.fgRed, '> Could not create the shows table.', error, Colors.fgReset);
--- src/components/Details.vue
+++ src/components/Details.vue
@@ -9,11 +9,12 @@
                 alt="Back"
             >
         </Button>
-        <img
-            class="details__cover"
-            :src="imgSrcRef"
-            alt="Cover"
-        >
+        <div class="details__cover">
+            <img
+                :src="imgSrcRef"
+                alt="Cover"
+            >
+        </div>
         <Button
             v-if="resultRef"
             additionalClass="details__add-btn"
@@ -89,6 +90,7 @@ export default defineComponent({
     grid-template-columns: auto minmax(400px, 1fr) auto;
     grid-template-rows: min-content minmax(auto, 1fr) 1fr;
     grid-column-gap: 1rem;
+
     .details__back-btn {
         grid-row-start: 1;
         grid-row-end: 1;
@@ -96,23 +98,36 @@ export default defineComponent({
         grid-column-end: 1;
         box-shadow:  5px 5px 15px #1a022f,
                     -5px -5px 15px #23023f;
+
         &:hover {
             box-shadow: inset 15px 15px 30px #1a022f,
                         inset -15px -15px 30px #23023f;
         }
+
         img {
             width: 4rem;
             height: auto;
         }
     }
+
     .details__cover {
         grid-row-start: 1;
         grid-row-end: 3;
         grid-column-start: 2;
         grid-column-end: 2;
-        width: 400px;
-        height: auto;
+        height: 0;
+        padding-bottom: 100%;
+        position: relative;
+
+        img {
+            position: absolute;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+        }
     }
+
     .details__add-btn, .details__remove-btn {
         grid-row-start: 1;
         grid-row-end: 1;
@@ -120,15 +135,18 @@ export default defineComponent({
         grid-column-end: 3;
         box-shadow:  5px 5px 15px #1a022f,
                     -5px -5px 15px #23023f;
+
         &:hover {
             box-shadow: inset 15px 15px 30px #1a022f,
                         inset -15px -15px 30px #23023f;
         }
+
         img {
             width: 4rem;
             height: auto;
         }
     }
+
     .details__info {
         grid-row-start: 3;
         grid-column-start: 2;
